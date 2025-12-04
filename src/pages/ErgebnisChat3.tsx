070655b24d3643cf35d95e21540a4edcb32b23fc
MileBlt <bluetner@helpcheck.de>
2025-12-04 15:48:34 +0100

    Update ErgebnisChat3 action labels

--- src/pages/ErgebnisChat3.tsx
+++ src/pages/ErgebnisChat3.tsx
@@ -283,7 +283,7 @@ const ErgebnisChat3 = () => {
               <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-3">
                 <div className="text-base text-brand-text/85">{footerNote}</div>
                 <Button asChild size="lg" className="w-full sm:w-auto" id="ergebnis-cta">
-                  <Link to="/beauftragen">Jetzt Stromrechnung prüfen</Link>
+                  <Link to="/beauftragen">Jetzt Geld zurückfordern</Link>
                 </Button>
               </div>
 
